42efed7cebd147e3723c6999a8d815ecb935055f
AMEEN TS <ameents1123@gmail.com>
2023-02-18 20:58:58 +0530

    four commit

--- src/API/BaseUrl.js
+++ src/API/BaseUrl.js
@@ -1,3 +1,3 @@
-export const BaseUrl="http://localhost:5000";
+// export const BaseUrl="http://localhost:5000";
 
-// export const BaseUrl="https://rich-plum-armadillo-coat.cyclic.app";
+export const BaseUrl="https://rich-plum-armadillo-coat.cyclic.app";
